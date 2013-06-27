cd9ec978b01b05ab822549c03e144a0665038580
prabhu <prabhu@sysadmin-HP-Pro-3090-Microtower-PC.(none)>
2013-06-27 11:40:29 +0530

    vender file added

---
+++ vendor/genemu/form-bundle/Genemu/Bundle/FormBundle/Twig/Extension/FormExtension.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the GenemuFormBundle package.
+ *
+ * (c) Olivier Chauvel <olivier@generation-multiple.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Genemu\Bundle\FormBundle\Twig\Extension;
+
+use Symfony\Bridge\Twig\Extension\FormExtension as BaseFormExtension;
+use Symfony\Component\Form\FormView;
+
+/**
+ * FormExtension extends Twig with form capabilities.
+ *
+ * @author Olivier Chauvel <olivier@generation-multiple.com>
+ */
+class FormExtension extends BaseFormExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            'form_javascript' => new \Twig_Function_Method($this, 'renderJavascript', array('is_safe' => array('html'))),
+            'form_stylesheet' => new \Twig_Function_Node('Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode', array('is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     * Render Function Form Javascript
+     *
+     * @param FromView $view
+     *
+     * @return string
+     */
+    public function renderJavascript(FormView $view, $prototype = false)
+    {
+        $block = $prototype ? 'javascript_prototype' : 'javascript';
+
+        return $this->renderer->searchAndRenderBlock($view, $block);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'genemu.twig.extension.form';
+    }
+}
